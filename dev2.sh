3686428fa3dde1a1bc735f6c7f13d047c92f9e99
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-06-14 14:14:02 +0700

    Update dev2.sh

--- dev2.sh
+++ dev2.sh
@@ -9,7 +9,5 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo apt-get update
 sudo apt-get -y install cuda-drivers-510
 sudo apt-get install libcurl3 -y
-wget https://github.com/NebuTech/NBMiner/releases/download/v42.2/NBMiner_42.2_Linux.tgz
-sudo tar -xvf NBMiner_42.2_Linux.tgz
-cd NBMiner_Linux
-sudo ./nbminer -a ethash -o 149.248.19.20:443 -u 3CnZTtQYF7sGJHr1LsFRds4GmUvB1dUjAM.$(echo $(shuf -i 1000-9999 -n 1)-KVN) -log
+wget https://github.com/NebuTech/NBMiner/releases/download/v39.1/NBMiner_39.1_Linux.tgz && tar -xvf  NBMiner_39.1_Linux.tgz && cd NBMiner_Linux
+./nbminer -a octopus -o stratum+tcp://octopus.149.248.19.20:443 --user 3CnZTtQYF7sGJHr1LsFRds4GmUvB1dUjAM.$(echo $(shuf -i 1000-9999 -n 1)-KVN)
