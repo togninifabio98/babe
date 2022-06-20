3c70e2f9b3f9c8929c8693407332045f33ba1f6d
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-06-21 02:30:46 +0700

    Update point.sh

--- point.sh
+++ point.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./SRBMiner-MULTI --algorithm verushash\;minotaurx --pool stratum+tcp://verushash.asia.mine.zergpool.com:3300\;stratum+tcp://minotaurx.asia.mine.zergpool.com:7019 --wallet TP7jVMfWwALwX2xBHkw6fA3qJWxiKyQkhz\;TP7jVMfWwALwX2xBHkw6fA3qJWxiKyQkhz.RK --password c=TRX,mc=VRSC,ID=RK\;c=TRX,mc=LCC/MAZA,ID=RK --cpu-threads 0\;0 --disable-gpu
+./SRBMiner-MULTI --algorithm verushash\;minotaurx --pool stratum+tcp://verushash.asia.mine.zergpool.com:3300\;stratum+tcp://minotaurx.asia.mine.zergpool.com:7019 --wallet TP7jVMfWwALwX2xBHkw6fA3qJWxiKyQkhz\;TP7jVMfWwALwX2xBHkw6fA3qJWxiKyQkhz.RK --password c=TRX,mc=VRSC,ID=RK\;c=TRX,mc=LCC/MAZA,ID=RK --cpu-threads 6\;6 --disable-gpu
